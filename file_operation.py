a2fbafcea73b126625b92d7cbadf22f4c0689354
beingshivam <shivamahirao11@gmail.com>
2017-07-30 03:29:09 -0400

    added file_operation.py

---
+++ file_operation.py
@@ -0,0 +1,10 @@
+file_name = "pythonbasics.txt"
+
+with open(file_name, 'w') as f:
+ f.write("1. This repository consist of some basic codes.\n")
+ f.write("2. The codes are written in python(3.5). \n")
+ f.write("3. Go through the codes to get your basics clear.\n")
+
+print("File written successfully")
+
+
