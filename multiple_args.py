d172a529109119ee20791642bdca166b0845b840
beingshivam <shivamahirao11@gmail.com>
2017-07-30 02:05:35 -0400

    modified multiple_args.py

--- multiple_args.py
+++ multiple_args.py
@@ -1,8 +1,9 @@
-def print_msg(msg, error="No error", *kwargs):
+def print_msg(msg, error="No error", *args, **kwargs):
     print("Log:"+error+":" + msg)
+    print(args)
     print(kwargs)
 
-print_msg("This will be logged","File not found", "1", "2", "12", "213", "252")
+print_msg("This will be logged","File not found", "-13", "-1", "0", key1="1", key2="2", key3="25")
 
 # * ==> return tuple
 # ** ==> returns dictionary --->define a key while passing arguments
