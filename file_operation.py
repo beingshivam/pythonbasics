556156737688edd06d519da926b3c12de77ea4c3
beingshivam <shivamahirao11@gmail.com>
2017-07-30 03:31:31 -0400

    modified file_operation.py

--- file_operation.py
+++ file_operation.py
@@ -7,4 +7,13 @@ with open(file_name, 'w') as f:
 
 print("File written successfully")
 
+#append
 
+"""
+
+with open(file_name, 'a') as f:
+ f.write("1. This repository consist of some basic codes.\n")
+ f.write("2. The codes are written in python(3.5). \n")
+ f.write("3. Go through the codes to get your basics clear.\n")
+
+"""
